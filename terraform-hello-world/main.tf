50292e01592050c6cb55cc76772553f2984f56a3
Ankitkhan08 <officialankitkhan@gmail.com>
2025-09-27 23:31:26 +0000

    feat: complete initial terraform hello-world

---
+++ terraform-hello-world/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}
+
+resource "local_file" "hello" {
+  filename = "${path.module}/hello.txt"
+  content  = "Hello, Terraform! I am now managing this file. Congratulations!"
+}
